d1185b06551cb9f282c21f676ba9a03c318d47eb
Gudrun Vene <gudrun.vene@khk.ee>
2019-09-26 11:58:25 +0300

    removed commenting from frontend under article folder

--- react-redux-realworld-example-app/src/components/Article/index.js
+++ react-redux-realworld-example-app/src/components/Article/index.js
@@ -1,5 +1,5 @@
 import ArticleMeta from './ArticleMeta';
-import CommentContainer from './CommentContainer';
+
 import React from 'react';
 import agent from '../../agent';
 import { connect } from 'react-redux';
--- react-redux-realworld-example-app/src/reducers/article.js
+++ react-redux-realworld-example-app/src/reducers/article.js
@@ -13,7 +13,7 @@ export default (state = {}, action) => {
       };
     case ARTICLE_PAGE_UNLOADED:
       return {};
- 
+
     default:
       return state;
   }
