986811222a626512c0bf2acf3dc1d691d7ef05d0
Gudrun Vene <gudrun.vene@khk.ee>
2019-09-26 15:09:28 +0300

    song to backend

--- node-express-realworld-example-app/app.js
+++ node-express-realworld-example-app/app.js
@@ -39,6 +39,7 @@ if(isProduction){
 
 require('./models/User');
 require('./models/Article');
+require('./models/Song');
 require('./config/passport');
 
 app.use(require('./routes'));
--- node-express-realworld-example-app/routes/api/index.js
+++ node-express-realworld-example-app/routes/api/index.js
@@ -3,6 +3,7 @@ var router = require('express').Router();
 router.use('/', require('./users'));
 router.use('/profiles', require('./profiles'));
 router.use('/articles', require('./articles'));
+router.use('/songs', require('./songs'));
 router.use('/tags', require('./tags'));
 
 router.use(function(err, req, res, next){
